2371b516e170dc9c78ce72b55a4e5459d45c2307
RskRasim <rasim.senyuz@outlook.com>
2019-02-27 22:38:59 +0300

    Wcf/Company.svc

---
+++ InventoryManagementWcfServiceIISHost/Company.svc.cs
@@ -0,0 +1,43 @@
+﻿using InventoryManagementInterfaces;
+using InventoryManagementEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using InventoryManagementBll.Concrete;
+using InventoryManagementDal.concrete.EntityFramework;
+
+namespace InventoryManagementWcfServiceIISHost
+{
+   
+    public class Company : ICompanyService
+    {
+        private CompanyManager _companyManeger = new CompanyManager(new EfCompanyDal());
+        public void Add(InventoryManagementEntity.Company company)
+        {
+            _companyManeger.Add(company);
+        }
+
+        public void Delete(int companyId)
+        {
+            _companyManeger.Delete(companyId);
+        }
+
+        public InventoryManagementEntity.Company Get(int companyId)
+        {
+           return _companyManeger.Get(companyId);
+        }
+
+        public List<InventoryManagementEntity.Company> GetAll()
+        {
+            return _companyManeger.GetAll();
+        }
+
+        public void Update(InventoryManagementEntity.Company company)
+        {
+            _companyManeger.Update(company);
+        }
+    }
+}
