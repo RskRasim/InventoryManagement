a3c6810700b94de21a6de48637268766ef7c6f3e
RskRasim <rasim.senyuz@outlook.com>
2019-03-05 13:57:41 +0300

    productImgCs Update

--- InventoryManagementEntity/ProductImg.cs
+++ InventoryManagementEntity/ProductImg.cs
@@ -11,7 +11,7 @@ namespace InventoryManagementEntity
     {
         public int Id { get; set; }
         public String Img { get; set; }
-        public int? ProductId { get; set; }
+        public int? ProductId { get; set; } 
 
         public  virtual Product Product { get; set; }
     }
