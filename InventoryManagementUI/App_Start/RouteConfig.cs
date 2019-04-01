e0ebdead4780d2b31aad84f3308c511c7cd441db
RskRasim <rasim.senyuz@outlook.com>
2019-04-01 15:21:12 +0300

    RouteConfig Update

--- InventoryManagementUI/App_Start/RouteConfig.cs
+++ InventoryManagementUI/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace InventoryManagementUI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Account", action = "CompanyLogin", id = UrlParameter.Optional }
             );
         }
     }
--- InventoryManagementUI/Controllers/HomeController.cs
+++ InventoryManagementUI/Controllers/HomeController.cs
@@ -11,45 +11,13 @@ namespace InventoryManagementUI.Controllers
 {
     public class HomeController : Controller
     {
-        StaffManager staffManager;
-        
-        public HomeController()
-        {
-            staffManager = new StaffManager(new EfStaffDal());
-        }
-
+     
        
         // GET: Home
         public ActionResult Index()
         {
-          List<Staff> staffs= staffManager.GetAll();
-
-            return View(staffs);
+            return View();
         }
-        /*Project Test - Staff*/
-        [HttpPost]
-        public ActionResult StaffAdd()
-        {
-            Staff staff = new Staff
-            {
-                Name =Request.Form["Name"],
-                Surname    =    Request.Form["Surname"],
-                Username = Request.Form["Username"],
-                Phone = Request.Form["Phone"],
-                Department = Request.Form["Department"],
-                Task = Request.Form["Task"],
-                Password = Request.Form["Password"],
-                CompanyId = 2
-                
-            };
-            staffManager.Add(staff);
-            return RedirectToAction("Index");
-        }
-       [HttpGet]
-       public ActionResult StaffDelete(int Id)
-        {
-            staffManager.Delete(Id);
-            return RedirectToAction("Index");
-        }
+      
     }
 }
